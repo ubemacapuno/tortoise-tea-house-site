8e40fd17c59f1523458f6f0d81a085180993a198
Corey Damocles <97559559+ubemacapuno@users.noreply.github.com>
2022-06-29 09:49:41 -0700

    Styling changes, work on nav

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,19 @@
 <script context="module" lang="ts">
+	import MobileHeader from '$lib/components/navbar/MobileHeader.svelte'
 	import Header from '$lib/components/header/Header.svelte';
 	import Footer from '$lib/components/footer/Footer.svelte';
 	import Contact from '$lib/components/contact/Contact.svelte';
-	import Hero from '$lib/components/hero/Hero.svelte';
 	// import MobileHeader from '$lib/components/navbar/MobileHeader.svelte';
 	import '../app.css';
 </script>
 
 <Header />
-<Hero />
 <main>
 	<slot />
 </main>
 <Contact />
 <Footer />
+
+<style>
+
+</style>
